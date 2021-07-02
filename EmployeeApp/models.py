dc4a35431939df4c3cd6edb059aae476140a8315
Anjali Upadhyay <ianjali1598@gmail.com>
2021-07-02 13:06:44 +0530

    changes made

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -16,15 +16,8 @@ class Employees(models.Model):
     # Position= models.ForeignKey(Position,on_delete=models.CASCADE)#ON DELETE CASCADE is used in to delete the
     #                                                               #rows from the child table automatically, when the rows 
     #  
-    Position=models.CharField(max_length=100)                                                             # from the parent table are deleted
+    Position=models.CharField(max_length=100)                      # from the parent table are deleted
     DateOfJoining = models.DateField()
     Contact = models.CharField(max_length=15)
    
-    # PhotoFileName = models.CharField(max_length=100)
-    # class Employee(models.Model):
-    # fullname = models.CharField(max_length=100)
-    # emp_code = models.CharField(max_length=3)
-    # mobile= models.CharField(max_length=15)
-    # position= models.ForeignKey(Position,on_delete=models.CASCADE)#ON DELETE CASCADE is used in to delete the
-    #                                                               #rows from the child table automatically, when the rows 
-    #                                                               # from the parent table are deleted
+   
